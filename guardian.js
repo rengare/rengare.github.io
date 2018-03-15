f35a74c91080cfe23d3bd7bee71e1a4e79b2f6a8
gorecki gracjan <gorecki.gracjan@hotmail.com>
2018-03-15 12:00:00 +0100

    push

--- guardian.js
+++ guardian.js
@@ -1,21 +1,22 @@
 (function() {
-    document.body.innerHTML = '';
-    var url = "http://rengare.github.io/";
-    // var url = "http://51.254.130.234:8085/";
-    // var url = "file:///home/ren/workspace/web/guardian/guardian/dist/";
-    // inline.bundle.js
-    var scripts = [
-        url + "inline.bundle.js",
-        url + "polyfills.bundle.js",
-        url + "main.bundle.js"
-    ]
+  document.body.innerHTML = "";
+  var url = "http://rengare.github.io/";
+  // var url = "http://51.254.130.234:8085/";
+  // var url = "file:///home/ren/workspace/web/guardian/guardian/dist/";
+  // inline.bundle.js
+  var scripts = [
+    url + "inline.bundle.js",
+    url + "polyfills.bundle.js",
+    url + "main.bundle.js",
+    url + "styles.bundle.css"
+  ];
 
-    var angularAppElement = document.createElement('app-root');
-    document.body.appendChild(angularAppElement);
+  var angularAppElement = document.createElement("app-root");
+  document.body.appendChild(angularAppElement);
 
-    for (var i = 0; i < scripts.length; i++) {
-        var s = document.createElement('script');
-        s.src = scripts[i];
-        document.body.appendChild(s);
-    }
-})()
+  for (var i = 0; i < scripts.length; i++) {
+    var s = document.createElement("script");
+    s.src = scripts[i];
+    document.body.appendChild(s);
+  }
+})();
